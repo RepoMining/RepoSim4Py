2fdee1b4c9f232fd4f67c609698f873900d8f2ae
liaomu <2376817023@qq.com>
2023-08-03 12:39:46 +0100

    Update example

---
+++ Script/Example.py
@@ -0,0 +1,12 @@
+import os
+from transformers import pipeline
+
+model = pipeline(
+    model="Henry65/RepoSim4Py",
+    trust_remote_code=True,
+    device_map="auto",
+    github_token=os.environ.get("GITHUB_TOKEN")
+)
+
+repo_infos = model("lazyhope/python-hello-world")
+print(repo_infos)
